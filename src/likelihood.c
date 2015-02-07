c81ccfc2ea8c0cab29076c0ffe51424a9657e30b
Jun Wei Ng <ngjw89@gmail.com>
2015-02-07 23:54:26 +0000

    comment

--- src/likelihood.c
+++ src/likelihood.c
@@ -14,6 +14,12 @@ mlgpStatus_t MLGP_LIKELIHOOD (
 )
 {
 
+  /* returns the negative log marginal likelihood of a GP p(y|X) if the
+   * NODERIVATIVES bit is not set in options.opts, derivatives of the log
+   * marginal likelihood with respect to the mean/covariance/noise will be
+   * computed and stored in mean.dparams/cov.dparams/lik.daparms
+   * */
+
   return MLGP_LIKELIHOOD_CPU(
                            nll,
                            X,
